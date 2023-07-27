315cd4298c4bf4ffa46386109c711f20b0cb1271
SMSourov <81343486+SMSourov@users.noreply.github.com>
2023-07-27 23:00:31 +0600

    Update yt-dlp.ps1
    Improved

--- yt-dlp.ps1
+++ yt-dlp.ps1
@@ -1,64 +1,157 @@
-$YouTube_video_URL = Read-Host "Enter your YouTube video URL"
-Write-Host "You have entered" $YouTube_video_URL
+$YouTube_URL = Read-Host "Enter your YouTube video URL"
 
-if ($IsWindows) {
-    # Firstly, download and convert the subtitle to SRT. SRT is supported in almost every video player that supports subtitle.
-    python yt-dlp $YouTube_video_URL --ffmpeg-location bin/ --write-subs --sub-langs all --convert-subs srt --skip-download
+# Get the info. The info file will be temporarily saved as "temp.info.json"
+py .\yt-dlp\ $YouTube_URL --write-info-json --skip-download --windows-filenames -o temp
+
+# In order to download a video, make sure that the height/width has one of the given values:
+# 4320
+# 2160
+# 1440
+# 1080
+# 720
+# 480
+# 360
+# 240
+# 144
+
+# Create variables for the width and height values
+$width = 1920
+# $height = 1080
+
+# Set the video codec, avaiable codecs "vp"(vp9), "av0"(av1), "avc"
+$vcodec = "vp"
+# $vcodec = "avc"
+# $vcodec = "av0"
+
+# Set the protocol, available codecs "https", "m3u8"
+$protocol = "https"
+# $protocol = "m3u8"
+
+# Set the audio codec, avaiable codecs "mp4a", "opus"
+# $acodec = "mp4a"
+$acodec = "opus"
+
+# Set the audio quality, available qualities are "ultralow", "low", "medium"
+# $format_note = "ultralow"
+# $format_note = "low"
+$format_note = "medium"
+
+# Making 2 flags to mark a prime id for video and audio is found
+$videoFound = 0
+$audioFound = 0
+
+# Set the optional commands
+# $optionalCommands = " -N 8 --windows-filenames --write-thumbnail --convert-thumbnails jpg --embed-thumbnail --embed-metadata --embed-chapters --ffmpeg-location bin/"
 
 
-    # Secondly, download and convert the subtitle to SSA. It has more features than SRT. To edit, a separate subtitle editor is recommended.
-    # python yt-dlp $YouTube_video_URL --ffmpeg-location bin/ --write-subs --sub-langs all --convert-subs ass --skip-download
+# Download the subtitles
+py .\yt-dlp\ "$YouTube_URL" --ffmpeg-location bin/ --write-subs --sub-langs all --convert-subs ass --skip-download
+py .\yt-dlp\ "$YouTube_URL" --ffmpeg-location bin/ --write-subs --sub-langs all --convert-subs srt --skip-download
 
 
-    # Thirdly, download the original subtitle files that are available in youtube
-    python yt-dlp $YouTube_video_URL --ffmpeg-location bin/ --write-subs --sub-langs all --skip-download
+# Read the JSON file and convert it to a PowerShell object
+$myJson = Get-Content -Raw temp.info.json | ConvertFrom-Json
 
+# Filter the formats array based on the variables
+$formats = $myJson.formats | Where-Object { 
+    $_.width -eq $width -and 
+    # $_.height -eq $height -and 
+    $_.vcodec -like "*$vcodec*" -and 
+    $_.protocol -like "*$protocol*" 
+}
+# Extract the format_id values and store them in an array variable
+$formatIds = $formats | Select-Object -ExpandProperty format_id
 
-    # Fourthly, download the video and others and embedd everything. Unfortunately, it only embeds the WebVTT subtitle file. You would have to use MKVToolNix to add the other subtitle files.
-    python yt-dlp $YouTube_video_URL -N 8 --windows-filenames --write-description --write-thumbnail --convert-thumbnails jpg --embed-thumbnail --embed-metadata --embed-chapters --xattrs --ffmpeg-location bin/
+# Check how many id $formatIds have. Only 1 is acceptable.
+$count = $formatIds.Count
+if ( $count -eq 1 ) {
+    $vidID = $formatIds.ToString()
+    $videoFound++
+    Write-Output "The video id is $vidID"
+}
+elseif ( $count -eq 0 ) {
 
+    # Filter the formats array based on the variables
+    $formats = $myJson.formats | Where-Object { 
+        # $_.width -eq $width -and 
+        $_.height -eq $width -and 
+        $_.vcodec -like "*$vcodec*" -and 
+        $_.protocol -like "*$protocol*" 
+    }
 
-    # Fifthly, if the video is 1080p or higher, download the 1080p video only. If you have a 1080p monitor/display, there is no meaning of having of a video that has a higher resolution of your monitor/display. Besides, a video of a higher(let's assume 8K) is uploaded, YouTube converts them into several lower resolution. One of them is 1080p. It will download the converted version. You may need to use MKVToolNix to edit the video file.
-    python yt-dlp $YouTube_video_URL -N 8 -f 248
+    # Extract the format_id values and store them in an array variable
+    $formatIds = $formats | Select-Object -ExpandProperty format_id
 
-
-    # Sixthly, open SubtitleEdit
-    SubtitleEdit.exe
-
-
-    # Seventhly, open MKVToolNix GUI
-    mkvtoolnix-gui.exe
+    # Check how many id $formatIds have. Only 1 is acceptable.
+    $count = $formatIds.Count
+    if ( $count -eq 1 ) {
+        $vidID = $formatIds.ToString()
+        $videoFound++
+        Write-Output "The video id is $vidID"
+    }
+    elseif ( $count -eq 0) {
+        Write-Output "The requested type of video is not available. You should the check the other quality."
+    }
+    elseif ( $count -gt 1) {
+        Write-Output "Multiple videos available. You need to be more specific.However, one is selected for you."
+        $vidID = $formatIds[0].ToString()
+        $videoFound++
+        Write-Output "The video id is $vidID"
+    }
+}
+elseif ( $count -gt 1) {
+    Write-Output "Multiple videos available. You need to be more specific. However, one is selected for you."
+    $vidID = $formatIds[0].ToString()
+    $videoFound++
+    Write-Output "The video id is $vidID"
 }
 
-if ($IsLinux) {
-    # Firstly, download and convert the subtitle to SRT. SRT is supported in almost every video player that supports subtitle.
-    python yt-dlp $YouTube_video_URL --write-subs --sub-langs all --convert-subs srt --skip-download
-
-
-    # Secondly, download and convert the subtitle to SSA. It has more features than SRT. To edit, a separate subtitle editor is recommended.
-    # python yt-dlp $YouTube_video_URL --write-subs --sub-langs all --convert-subs ass --skip-download
-
-
-    # Thirdly, download the original subtitle files that are available in youtube
-    python yt-dlp $YouTube_video_URL --write-subs --sub-langs all --skip-download
-
-
-    # Fourthly, download the video and others and embedd everything. Unfortunately, it only embeds the WebVTT subtitle file. You would have to use MKVToolNix to add the other subtitle files.
-    python yt-dlp $YouTube_video_URL -N 8 --windows-filenames --write-description --write-thumbnail --convert-thumbnails jpg --embed-thumbnail --embed-metadata --embed-chapters --xattrs
-
-
-    # Fifthly, if the video is 1080p or higher, download the 1080p video only. If you have a 1080p monitor/display, there is no meaning of having of a video that has a higher resolution of your monitor/display. Besides, a video of a higher(let's assume 8K) is uploaded, YouTube converts them into several lower resolution. One of them is 1080p. It will download the converted version. You may need to use MKVToolNix to edit the video file.
-    python yt-dlp $YouTube_video_URL -N 8 -f 248
-
-
-    # Sixthly, open SubtitleEdit
-    wine /mnt/sda2/Softwares/SE3610Beta/SubtitleEdit.exe
-
-
-    # Seventhly, open MKVToolNix GUI
-    flatpak run org.bunkus.mkvtoolnix-gui
-
+# Filter the formats array based on the variables
+$formats = $myJson.formats | Where-Object { 
+    $_.format_note -eq "$format_note" -and 
+    $_.acodec -like "*$acodec*" 
 }
+
+# Extract the format_id values and store them in an array variable
+$formatIds = $formats | Select-Object -ExpandProperty format_id
+
+# Check how many id $formatIds have. Only 1 is acceptable.
+$count = $formatIds.Count
+if ( $count -eq 1 ) {
+    $audID = $formatIds.ToString()
+    $audioFound++
+    Write-Output "The audio id is $audID"
+}
+elseif ( $count -eq 0 ) {
+    Write-Output "The requested type of audio is not available. You should the check the other quality."
+}
+elseif ( $count -gt 1) {
+    Write-Output "Multiple audios available. You need to be more specific. However, one is selected for you."
+    $audID = $formatIds[0].ToString()
+    $audioFound++
+    Write-Output "The audio id is $audID"
+}
+
+
+
+if ($videoFound -eq 1 -and $audioFound -eq 1) {
+    $audioVideoQuality = "$vidID+$audID"
+}
+elseif ($videoFound -eq 0 -and $audioFound -eq 1) {
+    $audioVideoQuality = "bv+$audID"
+}
+elseif ($videoFound -eq 1 -and $audioFound -eq 0) {
+    $audioVideoQuality = "$vidID+ba"
+}
+elseif ($videoFound -eq 0 -and $audioFound -eq 0) {
+    $audioVideoQuality = "bv+ba"
+}
+
+
+py .\yt-dlp\ "$YouTube_URL" -N 8 --windows-filenames --write-thumbnail --convert-thumbnails jpg  --embed-thumbnail --embed-metadata --embed-chapters --ffmpeg-location bin/ --write-subs --sub-format srt -f "$audioVideoQuality" 
+
+# Remove the temporary file
+Remove-Item temp.info.json
 
 
 Pause
