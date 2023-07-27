2fa5ef9216f9d01f88ae1ed03697dc1ecb78cc4f
SMSourov <81343486+SMSourov@users.noreply.github.com>
2023-07-27 22:27:06 +0600

    Update yt-dlp.windows.c

--- yt-dlp.windows.c
+++ yt-dlp.windows.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     // If you don't use/have Windows Terminal
-    // system("pwsh yt-dlp.ps1");
+    system("pwsh yt-dlp.ps1");
 
     // Firstly call the Windows Terminal.
     // Then before executing any command,
@@ -12,5 +12,5 @@ int main(void)
     // command will be executed in a another
     // tab. By opening the new tab, Windows
     // Terminal will not open the default tab.
-    system("wt nt pwsh yt-dlp.ps1");
+    // system("wt nt pwsh yt-dlp.ps1");
 }
