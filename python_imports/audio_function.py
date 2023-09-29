a576be124a0d4ed0be78b70edefd0fd243cf6961
SMSourov <81343486+SMSourov@users.noreply.github.com>
2023-09-29 07:34:09 +0600

    did something

--- python_imports/audio_function.py
+++ python_imports/audio_function.py
@@ -5,27 +5,11 @@ def audio_function(json_file, a_codec, format_note, a_lang):
     print("\n")
 
     formats = json_file['formats']
-    A_LANGS = {
-        "aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av", "ay", "az",
-        "ba", "be", "bg", "bh", "bi", "bm", "bn", "bo", "br", "bs", "ca", "ce", "ch",
-        "co", "cr", "cs", "cu", "cv", "cy", "da", "de", "dv", "dz", "ee", "el", "en",
-        "eo", "es", "et", "eu", "fa", "ff", "fi", "fj", "fo", "fr", "fy", "ga", "gd",
-        "gl", "gn", "gu", "gv", "ha", "he", "hi", "ho", "hr", "ht", "hu", "hy", "hz",
-        "ia", "id", "ie", "ig", "ii", "ik", "io", "is", "it", "iu", "ja", "jv", "ka",
-        "kg", "ki", "kj", "kk", "kl", "km", "kn", "ko", "kr", "ks", "ku", "kv", "kw",
-        "ky", "la", "lb", "lg", "li", "ln", "lo", "lt", "lu", "lv", "mg", "mh", "mi",
-        "mk", "ml", "mn", "mr", "ms", "mt", "my", "na", "nb", "nd", "ne", "ng", "nl",
-        "nn", "no", "nr", "nv", "ny", "oc", "oj", "om", "or", "os", "pa", "pi", "pl",
-        "ps", "pt", "qu", "rm", "rn", "ro", "ru", "rw", "sa", "sc", "sd", "se", "sg",
-        "si", "sk", "sl", "sm", "sn", "so", "sq", "sr", "ss", "st", "su", "sv", "sw",
-        "ta", "te", "tg", "th", "ti", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw",
-        "ty", "ug", "uk", "ur", "uz", "ve", "vi", "vo", "wa", "wo", "xh", "yi", "yo",
-        "za", "zh", "zu"
-}
+
     A_CODECS_REGEX = re.compile(r'^(opus|mp4a\.\d+\.\d+)$')
     FORMAT_NOTES = {"medium", "low", "ultralow"}
     a_codec_values = []
-    format_notes_values = []
+    format_note_values = []
 
     # Initialize boolean variables for format notes
     found_low = False
@@ -45,38 +29,87 @@ def audio_function(json_file, a_codec, format_note, a_lang):
             # Use regular expression to match 'a_codec_value' against the pattern
             if A_CODECS_REGEX.match(a_codec_value):
                 # Check if any of the FORMAT_NOTES is present in format_note_value
-                if not found_low and "low" in format_note_value.lower():
-                    found_low = True
-                if not found_ultralow and "ultralow" in format_note_value.lower():
-                    found_ultralow = True
                 if not found_medium and "medium" in format_note_value.lower():
                     found_medium = True
+                    format_note_values.append("medium")
+                if not found_low and "low" in format_note_value.lower():
+                    found_low = True
+                    format_note_values.append("low")
+                if not found_ultralow and "ultralow" in format_note_value.lower():
+                    found_ultralow = True
+                    format_note_values.append("ultralow")
 
                 # Add a_codec_value to the list if not present
                 if a_codec_value not in a_codec_values:
                     a_codec_values.append(a_codec_value)
 
             # Check if 'language' is in A_LANGS
-            if language_value in A_LANGS:
+            if language_value in video_properties.A_LANGS:
                 if language_value not in a_lang_values:
                     a_lang_values.append(language_value)
 
-    # Print which flags became true for format notes
-    if found_low:
-        print("Found 'low' in format notes.")
-    if found_ultralow:
-        print("Found 'ultralow' in format notes.")
-    if found_medium:
-        print("Found 'medium' in format notes.")
+    a_codec_values.sort()
+    total_codecs = len(a_codec_values)
 
     # Print the unique format notes, languages, and previously solved a_codec_values
-    print("\nUnique Format Notes:")
-    for format_note in list(set(a_codec_values)):
-        print(format_note)
+    print("\nTotal codecs", total_codecs)
+    print("\nAvailable audio codecs:")
+    i = 0
+    for codecs in a_codec_values:
+        print(i, " " + codecs)
+        i += 1
 
-    print("\nUnique Languages:")
+    i = 0
+    print("\nAvailable Languages:")
     for language in a_lang_values:
-        print(language)
+        print(i, " " + language)
+        i += 1
 
-        # don't make any changes to the prevous part.
-        # everything works like a charm
+    i = 0
+    print("\nAvailable audio qualities")
+    for format_note in format_note_values:
+        print(i, " " + format_note)
+        i += 1
+# don't make any changes to the prevous part.
+# everything works like a charm
+
+# Check if user's preferred properties are found in the filtered formats
+    if a_codec in a_codec_values:
+        print(f"Preferred audio codec '{a_codec}' found.")
+    else:
+        print(f"Preferred audio codec '{a_codec}' not found.")
+
+    if format_note.lower() in format_note_values:
+        print(f"Preferred format note '{format_note}' found.")
+    else:
+        print(f"Preferred format note '{format_note}' not found.")
+
+    if a_lang in a_lang_values:
+        print(f"Preferred audio language '{a_lang}' found.")
+    else:
+        print(f"Preferred audio language '{a_lang}' not found.")
+    
+    # User's preferred values
+    user_a_codec = 'mp4a.40.2'
+    user_format_note = 'low'
+    user_a_lang = 'en'
+
+    # Initialize a variable to store the matching format_id
+    matching_format_id = None
+
+    # Iterate through the formats to find a match
+    for format_info in formats:
+        if all(key in format_info for key in ['acodec', 'format_note', 'language']):
+            if (
+                format_info['acodec'] == user_a_codec and
+                format_info['format_note'].lower() == user_format_note.lower() and
+                format_info['language'] == user_a_lang
+            ):
+                matching_format_id = format_info['format_id']
+                break  # Stop searching after finding a match
+
+    # Check if a matching format_id was found
+    if matching_format_id:
+        print("Matching Format ID:", matching_format_id)
+    else:
+        print("No matching format found.")
